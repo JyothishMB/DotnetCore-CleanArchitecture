4f5c7073ff17aec925b2965df11fae8d05678f3d
Jyothish <jyothishmb1992@gmail.com>
2021-06-08 20:21:15 +0530

    Implemented the APi and fixed the sqlite path issue.

--- CleanArch/Api/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/Api/CleanArch.Api/Controllers/CourseController.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArch.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
+    
     public class CourseController : ControllerBase
     {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            Console.WriteLine("Here");
+            _courseService = courseService;
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+            return Ok(courseViewModel);
+        }
 
     }
 }
--- CleanArch/Api/CleanArch.Api/Startup.cs
+++ CleanArch/Api/CleanArch.Api/Startup.cs
@@ -2,10 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +36,19 @@ namespace CleanArch.Api
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArch.Api", Version = "v1" });
             });
+
+            Console.WriteLine(Configuration.GetConnectionString("UniversityDBConnection"));
+            
+            services.AddDbContext<UniversityDBContext>(options => 
+            {
+                options.UseSqlite(Configuration.GetConnectionString("UniversityDBConnection"),
+                b => b.MigrationsAssembly("CleanArch.Infra.Data"));
+            });
+            
+
+            services.AddMediatR(typeof(Startup));
+
+            RegisterServices(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +61,8 @@ namespace CleanArch.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CleanArch.Api v1"));
             }
 
+            
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
@@ -54,6 +73,11 @@ namespace CleanArch.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            DependencyContainer.RegisterServices(services);
         }
     }
 }
